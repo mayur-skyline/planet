c4658206ff354639531e6012c5beba4ae68b335b
mayur-skyline <mayur@skylineinfosys.com>
2023-11-08 17:20:31 +0530

    Typescript added

--- src/components/planets/index.tsx
+++ src/components/planets/index.tsx
@@ -8,8 +8,6 @@ type PlanetState = {
 };
 
 class Planet extends Component<PlanetProps, PlanetState> {
-
-
   render() {
     const data: { id: number; name: string }[] = [
       { id: 1, name: "Virani" },
@@ -20,32 +18,28 @@ class Planet extends Component<PlanetProps, PlanetState> {
       { id: 6, name: "virani3" },
     ];
 
-    return (<>
-      <div className="planet_box w-full h-screen flex justify-center items-center mx-auto">
+    return (
+      <>
+        <div className="planet_box w-full h-screen flex justify-center items-center mx-auto">
+          <div className="planet_box_container">
+            <div className="sun bg-gradient-to-r from-red-600 to-red-800 shadow-lg w-16 h-16 rounded-full absolute left-0 right-0 top-0 bottom-0 m-auto"></div>
 
-        <div className="planet_box_container">
-          <div className="sun bg-gradient-to-r from-red-600 to-red-800 shadow-lg w-16 h-16 rounded-full absolute left-0 right-0 top-0 bottom-0 m-auto"></div>
-
-          {data.map((d, index) => (
-            <div
-              key={d.id}
-              className="orbit absolute left-0 right-0 border border-solid border-black shadow-red rounded-full top-0 bottom-0 m-auto"
-              style={{
-                width: 100 + index * 50 + "px",
-                height: 100 + index * 50 + "px",
-                animation: `gravity ${5 + index * 3}s linear infinite`,
-              }}
-            >
+            {data.map((d, index) => (
               <div
-                className="circle cursor-pointer bg-gradient-to-r from-umber-600 to-umber-800 shadow-inner w-5 h-5 rounded-full absolute left-1/2 transform -translate-x-1/2 top-0"
-
-              ></div>
-            </div>
-          ))}
+                key={d.id}
+                className="orbit absolute left-0 right-0 border border-solid border-black shadow-red rounded-full top-0 bottom-0 m-auto"
+                style={{
+                  width: 100 + index * 50 + "px",
+                  height: 100 + index * 50 + "px",
+                  animation: `gravity ${5 + index * 3}s linear infinite`,
+                }}
+              >
+                <div className="circle cursor-pointer bg-gradient-to-r from-umber-600 to-umber-800 shadow-inner w-5 h-5 rounded-full absolute left-1/2 transform -translate-x-1/2 top-0"></div>
+              </div>
+            ))}
+          </div>
         </div>
-
-      </div></>
-
+      </>
     );
   }
 }
